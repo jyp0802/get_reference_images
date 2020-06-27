Fix loss masking

refineimage.py
@@ -1,3 +1,6 @@
+import sys
+if '/opt/ros/kinetic/lib/python2.7/dist-packages' in sys.path:
+	sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import os
 import torch 
 import torch.nn as nn
@@ -7,20 +10,32 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+
 import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
 
 from utils import get_model, show_image
+
+
+def bbox(img):
+	rows = np.any(img, axis=1)
+	cols = np.any(img, axis=0)
+	rmin, rmax = np.where(rows)[0][[0, -1]]
+	cmin, cmax = np.where(cols)[0][[0, -1]]
+	return rmin, rmax, cmin, cmax
 
 
 #### Configuration
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-num_epochs = 500
+num_epochs = 100
 batch_size = 16
 test_batch_size = 4
 learning_rate = 0.001
 
-data_path = "new_data"
+data_path = "combined_data"
+test_data_path = "test_data"
 save_path = "save_model"
 load, save_model = get_model(__file__, save_path)
 
@@ -36,6 +51,14 @@ class RefineImageDataset(Dataset):
 		self.mask_transform = transforms.Compose([
 			transforms.ToTensor()
 		])
+		self.to_pil_transform = transforms.Compose([transforms.ToPILImage()])
+
+		self.adjust_transform = transforms.Compose([
+				transforms.Resize((256,256)),
+				# transforms.RandomAffine(90, translate=None, scale=None, shear=10, resample=False, fillcolor=0),
+				transforms.ToTensor(),
+				transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+			])
 
 	def __len__(self):
 		return sum(os.path.isdir(os.path.join(self.root_dir, str(i))) for i in os.listdir(self.root_dir))
@@ -44,50 +67,79 @@ class RefineImageDataset(Dataset):
 		if torch.is_tensor(idx):
 			idx = idx.tolist()
 
-		# rgb with occlusion
-		# modal patch
-		# eraser
-		# erased patch
-		# predicted amodal patch
-		# predicted rgb
-		# ground truth rgb
+		# rgb with occlusion, modal patch, eraser, erased patch, predicted amodal patch, predicted rgb, ground truth rgb
 
 		imgs = []
 		for i in range(7):
 			npy_path = os.path.join(self.root_dir, str(idx), str(i)+".npy")
 			imgs.append(np.load(npy_path))
 
-		ref_path = os.path.join(self.root_dir, str(idx), "ref")
-		ref_npy = cv2.imread(ref_path)
-		ref_npy = cv2.cvtColor(ref_npy, cv2.COLOR_BGR2RGB)
+		# for x in imgs:
+		# 	img = Image.fromarray(x)
+		# 	plt.imshow(img)
+		# 	plt.show()
+		# 	plt.clf()
 
-		h, w, _ = ref_npy.shape
-		padding = (int(abs(h-w)/2), 0)
-		if h < w:
-			padding = padding[::-1]
+		#### ref image from google
+		# ref_path = os.path.join(self.root_dir, str(idx), "ref")
+		# ref_npy = cv2.imread(ref_path)
+		# ref_npy = cv2.cvtColor(ref_npy, cv2.COLOR_BGR2RGB)
 
-		ref_transform = transforms.Compose([
-			transforms.ToPILImage(),
-			transforms.Pad(padding, fill=(-1,-1,-1), padding_mode='constant'),
-			transforms.Resize(imgs[0].shape[:-1]),
-			transforms.ToTensor(),
-			transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-		])
+		# h, w, _ = ref_npy.shape
+		# padding = (int(abs(h-w)/2), 0)
+		# if h < w:
+		# 	padding = padding[::-1]
+
+		# ref_transform = transforms.Compose([
+		# 	transforms.ToPILImage(),
+		# 	transforms.Pad(padding, fill=(-1,-1,-1), padding_mode='constant'))
+		# ])
+
+		# ref = ref_transform(ref_npy)
+		# ref = self.adjust_transform(ref)
+		
+
+		#### ref image from adjusting gt image
+		gt_ref = imgs[6].copy()
+
+		if np.random.randint(2) == 0:
+			gt_ref = 255.0 * (gt_ref / 255.0)**np.random.uniform(0.4, 0.8, 1)[0]
+		else:
+			gt_ref = 255.0 * (gt_ref / 255.0)**np.random.uniform(1.3, 2.1, 1)[0]
+		gt_ref = gt_ref.astype(np.uint8)
+		
+		if np.random.randint(2) == 0:	
+			a,b,c,d = bbox(imgs[4])
+			a = np.random.randint(0,a+1)
+			b = np.random.randint(b,256)
+			c = np.random.randint(0,c+1)
+			d = np.random.randint(d,256)
+			gt_ref = gt_ref[a:b+1, c:d+1]
+			ref = self.to_pil_transform(gt_ref)
+			ref = self.adjust_transform(ref)
+		else:
+			padding = (np.random.randint(4,40),np.random.randint(4,40))
+			pad_transform = transforms.Compose([
+				transforms.ToPILImage(),
+				transforms.Pad(padding, padding_mode='edge')
+			])
+			ref = pad_transform(gt_ref)
+			ref = self.adjust_transform(ref)
 
 		rgb_occ = self.rgb_transform(imgs[0])
 		mod_mask = self.mask_transform(imgs[1])
+		eraser = self.mask_transform(imgs[2])
 		amod_mask = self.mask_transform(imgs[4])
 		rgb_pred = self.rgb_transform(imgs[5])
 		rgb_gt = self.rgb_transform(imgs[6])
-		ref = ref_transform(ref_npy)
-
-		sample = [rgb_occ, mod_mask, amod_mask, rgb_pred, ref, rgb_gt]
+	
+		sample = [rgb_occ, mod_mask, eraser, amod_mask, rgb_pred, ref, rgb_gt]
 
 		return sample
 
 
 trainset = RefineImageDataset(root_dir=data_path)
-trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=1)
 
 testset = RefineImageDataset(root_dir=data_path)
 testloader = DataLoader(testset, batch_size=test_batch_size, shuffle=True, num_workers=0)
@@ -98,7 +150,7 @@ class ConvAutoencoderNet(nn.Module):
 	def __init__(self):
 		super(ConvAutoencoderNet, self).__init__()
 		self.conv = nn.Sequential(
-			nn.Conv2d(11, 32, kernel_size=5, stride=1, padding=2),
+			nn.Conv2d(12, 32, kernel_size=5, stride=1, padding=2),
 			nn.ReLU(),
 			nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
 			nn.ReLU(),
@@ -131,22 +183,50 @@ def train():
 			for idx in range(len(inputs)):
 				inputs[idx] = inputs[idx].to(device)
 
-			rgb_occ, mod_mask, amod_mask, rgb_pred, ref, rgb_gt = inputs
+			rgb_occ, mod_mask, eraser, amod_mask, rgb_pred, ref, rgb_gt = inputs
+			mod_mask = (mod_mask > 0).float()
+			eraser = (eraser > 0).float()
+			amod_mask = (amod_mask > 0).float()
 
-			imgs_in = torch.cat(inputs[:-1], 1)
-			
-			# Forward pass
+			imgs_in = torch.cat([rgb_occ, mod_mask, eraser, amod_mask, rgb_pred, ref], 1)
+
+			#### Masked loss
 			rgb_out = model(imgs_in).to(device)
-			loss = criterion(rgb_out, rgb_gt)
-			
-			# Backward and optimize
+			loss = criterion(rgb_out*eraser, rgb_gt*eraser)
+			loss = (loss * eraser).sum()
+			mse_loss_val = loss / eraser.sum()
+
 			optimizer.zero_grad()
-			loss.backward()
-			optimizer.step()
+			mse_loss_val.backward()
+
+			# show_mask = (eraser).cpu().data.numpy()
+			# show_mask = np.transpose(show_mask, (0,2,3,1))
+			# show_mask = (show_mask * 255).astype(np.uint8)
+			# img = Image.fromarray(np.squeeze(show_mask[0]))
+			# plt.imshow(img)
+			# plt.show()
+			# plt.clf()
+			
+			# show_rgb = (rgb_gt*eraser).cpu().data.numpy()
+			# show_rgb = np.transpose(show_rgb, (0,2,3,1))
+			# show_rgb = (show_rgb * 255).astype(np.uint8)
+			# img = Image.fromarray(show_rgb[0])
+			# plt.imshow(img)
+			# plt.show()
+			# plt.clf()
+
+			
+			#### Whole image loss
+			# loss.backward()
+			# optimizer.step()
 			
 			if (epoch+1) % 1 == 0:
 				print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-					   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+					   .format(epoch+1, num_epochs, i+1, total_step, mse_loss_val.item()))
+
+		if (epoch+1) % 10 == 0:
+			torch.save(model.state_dict(), os.path.join(save_path, save_model[:-5]+'_'+str(epoch+1)+save_model[-5:]))
+
 
 
 def test():
@@ -159,7 +239,7 @@ def test():
 			for idx in range(len(inputs)):
 				inputs[idx] = inputs[idx].to(device)
 
-			rgb_occ, mod_mask, amod_mask, rgb_pred, ref, rgb_gt = inputs
+			rgb_occ, mod_mask, eraser, amod_mask, rgb_pred, ref, rgb_gt = inputs
 
 			imgs_in = torch.cat(inputs[:-1], 1)
 			

utils.py
@@ -57,14 +57,14 @@ def show_image(imgs, title=None, size=1):
 	if n > 1:
 		for i in range(n):
 			img = imgs[i] / 2 + 0.5  
-			npimg = np.transpose(img.numpy(), (1, 2, 0))
+			npimg = np.transpose(img.detach().numpy(), (1, 2, 0))
 			ax[i].imshow(npimg)
 			ax[i].axis('off')
 			if title != None:
 				ax[i].set_title(title[i])
 	else:
 		img = imgs[0] / 2 + 0.5  
-		npimg = np.transpose(img.numpy(), (1, 2, 0))
+		npimg = np.transpose(img.detach().numpy(), (1, 2, 0))
 		ax.imshow(npimg)
 		ax.axis('off')
 		if title != None: